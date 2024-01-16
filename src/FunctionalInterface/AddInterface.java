0d6fe56520e269c57209542e2d5bec97396845c4
rajendragurjar <gurjarr587@gmail.com>
2024-01-16 14:06:52 -0500

    L1 - Functional Interface Deafult or Basic Methods

--- src/FunctionalInterface/AddInterface.java
+++ src/FunctionalInterface/AddInterface.java
@@ -3,5 +3,10 @@ package FunctionalInterface;
 @FunctionalInterface
 public interface AddInterface {
     void add(int a,int b);
+
+    // it contain the basic java object class methods
+    int hashCode();
+    String toString();
+    boolean equals(Object obj);
 }
 
