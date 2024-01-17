cea0b50c0c09fbf3c97ee4566066ed6986114db4
rajendragurjar <gurjarr587@gmail.com>
2024-01-17 15:04:17 -0500

    L2 - Functional Interface + With Lambda Expression String Hello World

--- src/LambdaExpression/DriverClassHelloWorld.java
+++ src/LambdaExpression/DriverClassHelloWorld.java
@@ -2,7 +2,7 @@ package LambdaExpression;
 
 public class DriverClassHelloWorld {
     public static void main(String[] args){
-        HelloWorldInterface helloWorldInterface = (String name) -> {return "Hello" + name;};
+        HelloWorldInterface helloWorldInterface = (String name) -> {return "Hello " + name;};
         System.out.println(helloWorldInterface.myHelloWorld("Raj"));
     }
 }
