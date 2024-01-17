d3249ed1a496254a393d94b3f67975c093e73e52
rajendragurjar <gurjarr587@gmail.com>
2024-01-17 12:49:05 -0500

    L2 - Functional Interface + With Lambda Expression

--- src/FunctionalInterface/DriverClass.java
+++ src/FunctionalInterface/DriverClass.java
@@ -1,6 +1,6 @@
 package FunctionalInterface;
 
-public class Main {
+public class DriverClass {
     public static void main(String[] args){
         FunctionalInterfaceExample functionalInterfaceExample = new FunctionalInterfaceExample();
         functionalInterfaceExample.add(1,2);
--- src/LambdaExpression/DriverClassWithLambdaExp.java
+++ src/LambdaExpression/DriverClassWithLambdaExp.java
@@ -2,6 +2,10 @@ package LambdaExpression;
 
 public class DriverClassWithLambdaExp {
     public static void main(String[] args){
-
+        //With Lambda Expression
+        AddInterface addInterface = (a,b) -> {
+            System.out.println(a+b);
+        };
+        addInterface.add(3,4);
     }
 }
