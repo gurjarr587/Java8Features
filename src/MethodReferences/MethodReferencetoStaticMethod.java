ea67792d26111f707059365011c525013c778c6e
rajendragurjar <gurjarr587@gmail.com>
2024-01-18 15:36:30 -0500

    L6 - With Method Reference to intance of method

--- src/MethodReferences/MethodReferencetoStaticMethod.java
+++ src/MethodReferences/MethodReferencetoStaticMethod.java
@@ -1,11 +1,11 @@
-package LambdaMethodReferences;
+package MethodReferences;
 
-public class LambdaMethodReference {
+public class MethodReferencetoStaticMethod {
     public static void threadStatus(){
         System.out.println("Thread is running ...");
     }
     public static void main(String[] args){
-        Thread t2 = new Thread(LambdaMethodReference::threadStatus);
+        Thread t2 = new Thread(MethodReferencetoStaticMethod::threadStatus);
         t2.start();
     }
 }
